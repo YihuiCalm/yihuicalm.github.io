32654a700f56cd7e2f0b726066f29fd12ae480a1
YihuiCalm <96307958+YihuiCalm@users.noreply.github.com>
2026-04-06 18:55:38 +0000

    Deploying to gh-pages from @ YihuiCalm/yihuicalm.github.io@b13ffe9f15f833cba32c6f8709216da089519920 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "YihuiCalm/personal_website",
+    "data-repo": "YihuiCalm/yihuicalm.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
